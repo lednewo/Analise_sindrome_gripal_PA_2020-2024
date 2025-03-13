fd1d74454d7e0659bdb053b716b873987b279ded
Wendel Alves <euoendel@gmail.com>
2025-03-13 14:00:21 -0300

    first commit

---
+++ src/extract/exctrat_excel.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+input_file = 'src/sheets/casos.xlsx'
+output_file = 'src/outputs/dados_filtrados.xlsx'
+
+
+try:
+    df = pd.read_excel(input_file)
+
+    colunas = ["sintomas", "racaCor", "sexo", "estado", "municipio", "dataNotificacao"]
+    df_filtrado = df[colunas].dropna()
+
+    count_sintomas = df["sintomas"].dropna().value_counts().reset_index()
+
+    with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
+        df_filtrado.to_excel(writer, sheet_name="Dados Filtrados", index=False)
+        count_sintomas.to_excel(writer, sheet_name="Contagem Sintomas", index=False)
+
+    print(f"Arquivo salvo em: {output_file}")
+except Exception as e:
+    print(f"Erro ao processar o arquivo: {e}")
